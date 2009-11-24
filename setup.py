fac39989dd1ca3b18fe98b7455439a7d78772323
Robert Marianski <rob@marianski.com>
2009-11-24 18:32:55 -0500

    add simplejson as requirement if < 2.6

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
 import sys, os
 
+install_requires = []
+
+version_info = sys.version_info
+if version_info[0:1] == (2,) and version_info[1:2] < (6,):
+    install_requires.append('simplejson')
+
 version = '0.1'
 
 setup(name='python-bitly',
@@ -17,9 +23,7 @@ setup(name='python-bitly',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=install_requires,
       entry_points="""
       # -*- Entry points: -*-
       """,
